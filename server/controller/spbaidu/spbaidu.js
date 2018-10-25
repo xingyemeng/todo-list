28b87cdf1243c79e20cf6df2f7722f63d0756bba
ysx <15321451930@163.com>
2018-10-25 18:00:19 +0800

    20181025

--- server/controller/spbaidu/spbaidu.js
+++ server/controller/spbaidu/spbaidu.js
@@ -6,35 +6,41 @@ const async = require('async');
 
 const headers = {
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'Accept-Encoding': 'gzip, deflate, compress',
-  'Accept-Language': 'en-us;q=0.5,en;q=0.3',
-  'Cache-Control': 'max-age=0',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+  'Cache-Control': 'no-cache',
+  'Upgrade-Insecure-Requests': 1,
   'Connection': 'keep-alive',
-  'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0',
+  'Cookie': 'delPer=0; PSINO=1'
 }
 // 并发连接数的计数器
 let concurrencyCount = 0;
 
 
 var instance = axios.create({
-  baseURL: 'https://cnodejs.org/',
-  timeout: 10000,
+  timeout: 1000,
   headers: headers,
   maxRedirects: 0
 });
 
 module.exports = {
-  spider (wd) {
-    let cnodeUrl = encodeURI('https://www.baidu.com/s?wd=' + wd + '&pn=');
-    const arr = []
-    const arrTitle = []
-    const parsedUrl = []
-    let count = 1;
-    let pageNumbers = [0,10,20];
+  count: 0,
+  parsedUrl: [],
+  spider (str) {
     const that = this
+    return new Promise((resolve, reject) => {
+      let cnodeUrl = ''
+      ++(this.count)
+      if(this.count ==1) {
+        cnodeUrl = encodeURI('https://www.baidu.com' + str);
+      }else{
+        cnodeUrl = 'https://www.baidu.com' + str;
+      }
+      const arr = []
+      const arrTitle = []
       instance.get(cnodeUrl)
         .then(function(res){
-          console.log(res.data)
           const $ = cheerio.load(res.data);
           $('.c-container>h3>a').each(function () {
             arr.push($(this).attr('href'))
@@ -42,17 +48,21 @@ module.exports = {
           $('.c-container>h3>a').each(function () {
             arrTitle.push($(this).text())
           })
-          console.log(arrTitle)
-          console.log(arr)
           that.parseUrl(arr).then(res => {
-            parsedUrl.push(...res);
-            console.log(parsedUrl)
+            that.parsedUrl.push(...res);
+            if (that.count < 3) {
+              let aHref = $('#page>strong+a').attr('href')
+              that.spider(aHref).then(res=>{resolve(res)})
+            }else {
+              that.count = 0
+              resolve(that.parsedUrl)
+              that.parsedUrl = []
+            }
           })
         }).catch(err => {
-        console.error(err)
-      })
-
-
+          reject(err)
+        })
+    })
   },
    parseUrl (arr) {
     return new Promise((resolve, reject) => {
@@ -61,12 +71,47 @@ module.exports = {
           .then(res => {
 
           }).catch(err => {
-          callback(null,err.response.headers.location)
-        })
+            if(item.indexOf('http') !== -1){
+              callback(null,err.response.headers.location)
+            }else{
+              callback(null,'http://www.baidu.com/' + item)
+            }
+
+          })
       },function (err, results) {
         if(err) reject(err)
         resolve(results)
       })
+    })
+  },
+  /**
+   * 获取前三页的搜索内容
+   *
+   * */
+  getAllData (arrTitle, parsedUrl) {
+    const that = this
+    instance.get(cnodeUrl)
+      .then(function(res){
+        const $ = cheerio.load(res.data);
+        $('.c-container>h3>a').each(function () {
+          arr.push($(this).attr('href'))
+        })
+        $('.c-container>h3>a').each(function () {
+          arrTitle.push($(this).text())
+        })
+        console.log(arrTitle)
+        console.log(arr)
+        that.parseUrl(arr).then(res => {
+          parsedUrl.push(...res);
+          console.log(parsedUrl)
+          if (that.count < 3) {
+            console.log($('#page>strong+a').attr('href'))
+            let aHref = $('#page>strong+a').attr('href')
+            that.spider(aHref)
+          }
+        })
+      }).catch(err => {
+      console.error(err)
     })
   }
 }
--- server/router/spbaidu/spbaidu.js
+++ server/router/spbaidu/spbaidu.js
@@ -4,8 +4,15 @@ const spbaidu = require('../../controller/spbaidu/spbaidu')
 const router = express.Router();
 router.get('/wtlist', function (req,res,next) {
   console.log(req.query.wd)
-
-  spbaidu.spider('百达翡丽维修')
+  let wd = '百达翡丽维修'
+  let pn = 0
+  const str = '/s?wd=' + wd + '&pn=' + pn + '&usm=1'
+  spbaidu.spider(str).then(resolve => {
+      res.send(resolve)
+      console.log(123)
+    }).catch(err=>{
+      console.log(err)
+    })
 })
 
 module.exports = router
